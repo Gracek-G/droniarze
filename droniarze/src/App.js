77579ac605ddbb6a2860752bd9972c82dd5c6d04
argsk13 <103596153+argsk13@users.noreply.github.com>
2024-10-08 22:26:55 +0200

    add landing page scaffold

---
+++ droniarze/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          class="text-amber-400"
+          className="App-link"
+          href="https://www.skyscapist.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sky rapist
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
